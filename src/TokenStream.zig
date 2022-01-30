c4f5927d41fbaecd5333e75d08507871ccb76088
InKryption <inkryption07@gmail.com>
2022-01-30 14:24:17 +0100

    First stubs in the actual function

--- src/TokenStream.zig
+++ src/TokenStream.zig
@@ -1,4 +1,10 @@
 const std = @import("std");
+const mem = std.mem;
+const meta = std.meta;
+const debug = std.debug;
+const testing = std.testing;
+
+const assert = debug.assert;
 
 const validate_slice = @import("validate_slice.zig");
 const utility = @import("utility.zig");
@@ -6,6 +12,19 @@ const TagTokenizer = @import("TagTokenizer.zig");
 
 const TokenStream = @This();
 frame: @Frame(TokenStream.tokenize) = undefined,
+tok: *?Tok,
+
+pub const Error = error{};
+
+pub fn next(ts: *TokenStream) ?Tok {
+    var tok: ?Tok = null;
+
+    ts.tok = &tok;
+    defer ts.tok = undefined;
+
+    resume ts.frame;
+    return tok;
+}
 
 pub fn reset(ts: *TokenStream, src: []const u8) validate_slice.ValidateSliceResult {
     const validation_result = validate_slice.validateUtf8Slice(src);
@@ -21,22 +40,67 @@ pub fn resetUnchecked(ts: *TokenStream, src: []const u8) void {
     ts.frame = async ts.tokenize(src);
 }
 
-pub const Tok = union(enum) {
-    stub
+pub const Tok = struct {
+    index: usize,
+    info: Info,
+
+    pub fn init(index: usize, comptime id: Id, expr: anytype) Tok {
+        return .{
+            .index = index,
+            .info = @unionInit(Info, @tagName(id), expr),
+        };
+    }
+
+    pub const Id = meta.Tag(Info);
+    pub const Info = union(enum) {
+        err: Err,
+
+        comment: Len,
+
+        pub const Err = struct { code: Error };
+        pub const Len = struct { len: usize };
+    };
 };
 
 fn tokenize(ts: *TokenStream, src: []const u8) void {
     var i: usize = 0;
     var tt: TagTokenizer = .{};
+    suspend {}
+
     _ = ts;
     _ = src;
     _ = i;
     _ = tt;
-    suspend {}
 
-    tokenize: {
-        i = utility.nextNonXmlWhitespaceCharIndexAfter(src, i);
-        break :tokenize;
+    tokenization: {
+        i = utility.nextNonXmlWhitespaceCharIndexAfter(src[i..], i);
+        tt.reset(src[i..]).unwrap() catch unreachable;
+        if (tt.next()) |tok| switch (tok.info) {
+            .err => debug.todo("Implement 'err' branch."),
+            .comment_start => debug.todo("Implement 'comment_start' branch."),
+            .cdata_start => debug.todo("Implement 'cdata_start' branch."),
+            .pi_start => debug.todo("Implement 'pi_start' branch."),
+            .elem_open_start => debug.todo("Implement 'elem_open_start' branch."),
+            .elem_close_start => debug.todo("Implement 'elem_close_start' branch."),
+
+            else => if (@import("builtin").mode == .Debug) {
+                const longest_tok_name = comptime @tagName(std.enums.values(TagTokenizer.Tok.Id)[
+                    std.sort.argMax(
+                        TagTokenizer.Tok.Id,
+                        std.enums.values(TagTokenizer.Tok.Id),
+                        void{},
+                        struct {
+                            fn lessThan(_: void, lhs: TagTokenizer.Tok.Id, rhs: TagTokenizer.Tok.Id) bool {
+                                return mem.lessThan(u8, @tagName(lhs), @tagName(rhs));
+                            }
+                        }.lessThan,
+                    ).?
+                ]);
+                const format = "Didnt expect tag token '{s}'.";
+                var buf: [std.fmt.count(format, .{longest_tok_name})]u8 = undefined;
+                @panic(std.fmt.bufPrint(&buf, format, .{@tagName(tok.info)}) catch unreachable);
+            } else unreachable,
+        } else break :tokenization;
     }
 
     while (true) {
@@ -67,6 +131,6 @@ const tests = struct {
 
 test {
     var ts = tests.TestTokenStream{};
-    
+
     ts.reset("<?xml version=\"1.0\" encoding=\"UTF-8\"?>").unwrap() catch unreachable;
 }

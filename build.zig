51ada158c59ec38ca9a741d765485ab57132c0c0
InKryption <inkryption07@gmail.com>
2022-03-18 22:30:29 +0100

    Added test-filter option

--- build.zig
+++ build.zig
@@ -7,6 +7,7 @@ pub fn build(b: *std.build.Builder) void {
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
     main_tests.setTarget(target);
+    main_tests.setFilter(b.option([]const u8, "test-filter", "Filter for the tests"));
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);

c24d56e5aeaf4fd17ab9d0f63de309a940463068
InKryption <inkryption07@gmail.com>
2022-03-21 12:48:33 +0100

    Change stuff in accordance with changes in TagTokenizer, and re-think plan in order to naively track depth

--- src/TokenStream.zig
+++ src/TokenStream.zig
@@ -54,7 +54,7 @@ pub const Tok = struct {
         whitespace: Len,
 
         /// indicates '<?{name}'
-        pi: Len,
+        pi_start: Len,
         /// indicates a non-string token following 'pi_target', 'pi_str', or 'pi_tok'
         pi_tok: Len,
         /// indicates a string following 'pi_target', 'pi_tok', or 'pi_str'
@@ -83,17 +83,18 @@ pub const Tok = struct {
         pub const Len = struct { len: usize };
         pub const ElementClose = struct {
             len: usize,
-            name_index: usize,
-            name_len: usize,
+            name: Name,
+
+            pub const Name = struct { index: usize, len: usize };
         };
     };
 
     pub fn slice(tok: Tok, src: []const u8) []const u8 {
-        return src[tok.index .. tok.index + tok.len()];
+        return src[tok.index..][0..tok.len()];
     }
 
     pub fn piTarget(tok: Tok, src: []const u8) ?[]const u8 {
-        if (tok.info != .pi) return null;
+        if (tok.info != .pi_start) return null;
         return tok.slice(src)["<?".len..];
     }
 
@@ -131,7 +132,7 @@ pub const Tok = struct {
         return switch (tok.info) {
             .err => unreachable,
             .whitespace => |whitespace| whitespace.len,
-            .pi => |pi| pi.len,
+            .pi_start => |pi_start| pi_start.len,
             .pi_tok => |pi_tok| pi_tok.len,
             .pi_str => |pi_str| pi_str.len,
             .comment => |comment| comment.len,
@@ -150,49 +151,89 @@ fn tokenize(ts: *TokenStream, src: []const u8) void {
     var tag_tokenizer: TagTokenizer = .{};
     suspend {}
 
-    _ = ts;
-    _ = src;
-    _ = i;
-    _ = tag_tokenizer;
+    prolog_tokenization: while (true) {
+        i = utility.nextNonXmlWhitespaceCharIndexAfter(src, i);
+        tag_tokenizer.resetUnchecked(src[i..]);
 
-    tokenization: {
-        i = utility.nextNonXmlWhitespaceCharIndexAfter(src[i..], i);
+        if (tag_tokenizer.next()) |start_tag_tok| {
+            switch (start_tag_tok.info) {
+                .pi_open => {
+                    tokenize_pi: {
+                        const pi_target = tag_tokenizer.next() orelse std.debug.todo("Error here.");
+                        if (pi_target.info != .pi_target) std.debug.todo("Error here.");
 
-        while (true) {
-            tag_tokenizer.reset(src[i..]).unwrap() catch unreachable;
-            if (tag_tokenizer.next()) |start_tag_tok| {
-                switch (start_tag_tok.info) {
-                    .err => std.debug.todo("Implement 'err' branch."),
+                        const len: usize = pi_target.index + pi_target.info.pi_target.len;
+                        suspend ts.emitResult(i, .pi_start, .{ .len = len });
 
-                    .comment_start => {
-                        const next_tag_tok = tag_tokenizer.next() orelse std.debug.todo("Handle no token following comment_start.");
-                        if (next_tag_tok.info != .comment_text) std.debug.todo("Handle non-comment_text token following comment_start.");
+                        break :tokenize_pi;
+                    }
 
-                        const last_tag_tok = tag_tokenizer.next() orelse std.debug.todo("Handle no token following comment_text.");
-                        if (last_tag_tok.info != .comment_end) std.debug.todo("Handle non-comment_end token following commtent_text.");
+                    while (tag_tokenizer.next()) |pi_tok_str_end| {
+                        switch (pi_tok_str_end.info) {
+                            .pi_tok => |pi_tok| {
+                                const len: usize = pi_tok.len;
+                                suspend ts.emitResult(i + pi_tok_str_end.index, .pi_tok, .{ .len = len });
+                            },
+                            .pi_str => |pi_str| {
+                                const len: usize = pi_str.len;
+                                suspend ts.emitResult(i + pi_tok_str_end.index, .pi_str, .{ .len = len });
+                            },
+                            .pi_close => {
+                                i += pi_tok_str_end.index + pi_tok_str_end.info.cannonicalSlice().?.len;
+                                continue :prolog_tokenization;
+                            },
+                            else => std.debug.todo("Error here."),
+                        }
+                    } else std.debug.todo("Error here.");
+                },
+                .pi_target,
+                .pi_tok,
+                .pi_str,
+                .pi_close,
+                => unreachable,
 
-                        const len = len: {
-                            var len: usize = 0;
-                            len += start_tag_tok.info.cannonicalSlice().?.len;
-                            len += next_tag_tok.info.comment_text.len;
-                            len += last_tag_tok.info.cannonicalSlice().?.len;
-                            break :len len;
-                        };
-                        suspend ts.tok.* = Tok.init(i, .comment, .{ .len = len });
-                        i += len;
-                    },
+                .comment_start => {
+                    const comment_text = tag_tokenizer.next() orelse std.debug.todo("Error here.");
+                    if (comment_text.info != .comment_text) std.debug.todo("Error here.");
 
-                    .cdata_start => std.debug.todo("Implement 'cdata_start' branch."),
-                    .pi_start => std.debug.todo("Implement 'pi_start' branch."),
-                    .elem_open_start => std.debug.todo("Implement 'elem_open_start' branch."),
-                    .elem_close_start => std.debug.todo("Implement 'elem_close_start' branch."),
+                    const comment_end = tag_tokenizer.next() orelse std.debug.todo("Error here.");
+                    if (comment_end.info != .comment_end) std.debug.todo("Error here.");
 
-                    else => didNotExpectTagToken(start_tag_tok.info),
-                }
-            } else std.debug.todo("Implement this branch");
-        }
+                    const len: usize = comment_end.index + comment_end.info.cannonicalSlice().?.len;
+                    suspend ts.emitResult(i, .comment, .{ .len = len });
+                    i += len;
+                },
+                .comment_text,
+                .comment_end,
+                => unreachable,
 
-        break :tokenization;
+                .cdata_start => std.debug.todo("Error here."),
+                .cdata_text,
+                .cdata_end,
+                => unreachable,
+
+                .elem_open_start => {
+                    const elem_tag_name = tag_tokenizer.next() orelse std.debug.todo("Error here.");
+                    if (elem_tag_name.info != .elem_tag_name) std.debug.todo("Error here.");
+
+                    const len: usize = elem_tag_name.index + elem_tag_name.info.elem_tag_name.len;
+                    suspend ts.emitResult(i, .elem_open, .{ .len = len });
+                    i += len;
+
+                    break :prolog_tokenization;
+                },
+                .elem_close_start => std.debug.todo("Error here."),
+                .elem_close_inline => unreachable,
+                .elem_tag_end => unreachable,
+                .elem_tag_name => unreachable,
+
+                else => std.debug.todo("Do the rest"),
+            }
+        } else break :prolog_tokenization;
+    }
+
+    body_tokenization: while (true) {
+        break :body_tokenization;
     }
 
     while (true) {
@@ -200,61 +241,110 @@ fn tokenize(ts: *TokenStream, src: []const u8) void {
     }
 }
 
-fn didNotExpectTagToken(tag_tok: TagTokenizer.Tok.Id) noreturn {
-    if (@import("builtin").mode != .Debug)
-        unreachable
-    else {
-        const format = "Didnt expect tag token '{s}'.\n";
-        var buf: [std.fmt.count(format, .{utility.longestEnumName(TagTokenizer.Tok.Id)})]u8 = undefined;
-        @panic(std.fmt.bufPrint(&buf, format, .{@tagName(tag_tok)}) catch unreachable);
-    }
+fn emitResult(ts: *TokenStream, index: usize, comptime id: Tok.Id, expr: anytype) void {
+    std.debug.assert(ts.tok.* == null);
+    ts.tok.* = Tok.init(index, id, expr);
 }
 
-const tests = struct {
-    const TestTokenStream = struct {
-        ts: TokenStream = .{},
-        src: []const u8 = &.{},
+fn emitError(ts: *TokenStream, index: usize, code: Error) void {
+    ts.emitResult(index, .err, .{ .code = code });
+}
 
-        fn reset(test_ts: *TestTokenStream, src: []const u8) validate_slice.ValidateSliceResult {
-            test_ts.src = src;
-            return test_ts.ts.reset(test_ts.src);
-        }
+const TestTokenStream = struct {
+    ts: TokenStream = .{},
+    src: []const u8 = &.{},
 
-        fn resetUnchecked(test_ts: *TestTokenStream, src: []const u8) void {
-            test_ts.src = src;
-            test_ts.ts.resetUnchecked(test_ts.src);
-        }
+    fn reset(test_ts: *TestTokenStream, src: []const u8) validate_slice.ValidateSliceResult {
+        test_ts.src = src;
+        return test_ts.ts.reset(test_ts.src);
+    }
 
-        fn next(test_ts: *TestTokenStream) ?Tok {
-            return test_ts.ts.next();
-        }
+    fn resetUnchecked(test_ts: *TestTokenStream, src: []const u8) void {
+        test_ts.src = src;
+        test_ts.ts.resetUnchecked(test_ts.src);
+    }
 
-        fn expectComment(test_ts: *TestTokenStream, text: []const u8) !void {
-            const tok = test_ts.next() orelse return error.TestExpectedEqual;
-            try std.testing.expectEqual(Tok.Id.comment, tok.info);
-            try std.testing.expectEqualStrings(text, tok.commentText(test_ts.src).?);
+    fn next(test_ts: *TestTokenStream) ?Tok {
+        return test_ts.ts.next();
+    }
 
-            const expected_slice = try std.mem.concat(std.testing.allocator, u8, &.{ "<!--", text, "-->" });
-            defer std.testing.allocator.free(expected_slice);
+    fn expectComment(test_ts: *TestTokenStream, text: []const u8) !void {
+        const tok = test_ts.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.comment, tok.info);
+        try std.testing.expectEqualStrings(text, tok.commentText(test_ts.src).?);
 
-            try std.testing.expectEqualStrings(expected_slice, tok.slice(test_ts.src));
-        }
+        const expected_slice = try std.mem.concat(std.testing.allocator, u8, &.{ "<!--", text, "-->" });
+        defer std.testing.allocator.free(expected_slice);
 
-        fn expectErr(test_tt: *TestTokenStream, err: TokenStream.Error) !void {
-            const tok = test_tt.next() orelse return error.TestExpectedEqual;
-            try std.testing.expectEqual(Tok.Id.err, tok.info);
-            try std.testing.expectEqual(err, tok.info.err.code);
-        }
-        fn expectNull(test_tt: *TestTokenStream) !void {
-            try std.testing.expectEqual(@as(?Tok, null), test_tt.next());
-        }
-    };
+        try std.testing.expectEqualStrings(expected_slice, tok.slice(test_ts.src));
+    }
+
+    fn expectCdata(test_ts: *TestTokenStream, text: []const u8) !void {
+        const tok = test_ts.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.cdata, tok.info);
+        try std.testing.expectEqualStrings(text, tok.cdataText(test_ts.src).?);
+
+        const expected_slice = try std.mem.concat(std.testing.allocator, u8, &.{ "<![CDATA[", text, "]]>" });
+        defer std.testing.allocator.free(expected_slice);
+
+        try std.testing.expectEqualStrings(expected_slice, tok.slice(test_ts.src));
+    }
+
+    fn expectPiStart(test_ts: *TestTokenStream, name: []const u8) !void {
+        const tok = test_ts.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.pi_start, tok.info);
+        try std.testing.expectEqualStrings(name, tok.piTarget(test_ts.src).?);
+
+        const expected_slice = try std.mem.concat(std.testing.allocator, u8, &.{ "<?", name });
+        defer std.testing.allocator.free(expected_slice);
+
+        try std.testing.expectEqualStrings(expected_slice, tok.slice(test_ts.src));
+    }
+
+    fn expectPiTok(test_ts: *TestTokenStream, slice: []const u8) !void {
+        const tok = test_ts.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.pi_tok, tok.info);
+        try std.testing.expectEqualStrings(slice, tok.slice(test_ts.src));
+    }
+
+    fn expectPiStr(test_ts: *TestTokenStream, text: []const u8) !void {
+        const tok = test_ts.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.pi_str, tok.info);
+        try std.testing.expectEqualStrings(text, tok.piStrText(test_ts.src).?);
+
+        const actual_slice = tok.slice(test_ts.src);
+
+        try std.testing.expect(switch (actual_slice[0]) {
+            '\'', '\"' => true,
+            else => false,
+        });
+        try std.testing.expect(0 != (actual_slice.len - 1));
+        try std.testing.expectEqual(actual_slice[0], actual_slice[actual_slice.len - 1]);
+
+        const expected_slice = try std.mem.concat(std.testing.allocator, u8, &.{ actual_slice[0..1], text, actual_slice[actual_slice.len - 1 ..][0..1] });
+        defer std.testing.allocator.free(expected_slice);
+
+        try std.testing.expectEqualStrings(expected_slice, actual_slice);
+    }
+
+    fn expectErr(test_tt: *TestTokenStream, err: TokenStream.Error) !void {
+        const tok = test_tt.next() orelse return error.TestExpectedEqual;
+        try std.testing.expectEqual(Tok.Id.err, tok.info);
+        try std.testing.expectEqual(err, tok.info.err.code);
+    }
+    fn expectNull(test_tt: *TestTokenStream) !void {
+        try std.testing.expectEqual(@as(?Tok, null), test_tt.next());
+    }
 };
 
-test "TokenStream Lone Comment Token" {
-    var ts = tests.TestTokenStream{};
+test "TokenStream Basic Usages" {
+    var ts = TestTokenStream{};
 
-    ts.reset("<!-- foo -->").unwrap() catch unreachable;
+    ts.reset("<!-- foo --><?foo bar 'baz'?><!-- fizz -->").unwrap() catch unreachable;
     try ts.expectComment(" foo ");
+    try ts.expectPiStart("foo");
+    try ts.expectPiTok("bar");
+    try ts.expectPiStr("baz");
+    try ts.expectComment(" fizz ");
     try ts.expectNull();
 }

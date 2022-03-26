47e42c22b0c7bd69531b3cfc35afc4e2b75b9add
InKryption <inkryption07@gmail.com>
2022-03-26 02:29:18 +0100

    Swap out files so tests get run in the file currently being developed

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,9 @@
 pub const ValidateSliceResult = @import("validate_slice.zig").ValidateSliceResult;
 pub const TagTokenizer = @import("TagTokenizer.zig");
-pub const TokenStream = @import("TokenStream.zig");
+pub const TokenStream = @import("TokenStream2.zig");
 
 comptime {
     _ = ValidateSliceResult;
-    _ = TagTokenizer;
+    // _ = TagTokenizer;
     _ = TokenStream;
 }
